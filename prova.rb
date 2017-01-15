292c7e35bf237b66223804e56caf02946977db06
fabio <fabiopozzi@fastmail.com>
2017-01-15 12:48:52 +0100

    Create data array. Added a date filter.
    . create a data array containing an hash for each record.
      Include only attributes of interest
    . implemented a simple date filter to get an array with rows
      related to a particular month

--- prova.rb
+++ prova.rb
@@ -1,5 +1,26 @@
 require 'csv'
+require 'date'
 
-CSV.foreach('aaa.csv', {:col_sep => ';', :headers => true}) do |row|
-    puts row['Titolo']
+# takes a date and data array
+# returns a new array containing rows with matching year and month
+def filter_by_month(date, records)
+  filt_records = records.map{ |x| x if x[:data].month == date.month &&
+    x[:data].year == date.year }.compact
 end
+
+
+data = Array.new
+
+CSV.foreach('aaa.csv', col_sep: ';', headers: true) do |row|
+    t = Hash.new
+    t[:titolo] = row['Titolo']
+    t[:data] = Date::strptime(row['Data'], "%d\/%m\/%Y")
+    data << t
+end
+
+# filtro per nome titolo
+#tim_array = data.map{ |x| x if x[:titolo] == 'tim' }
+#puts tim_array.compact
+
+dati_dicembre = filter_by_month(Date.new(2016,12,1), data)
+puts dati_dicembre
