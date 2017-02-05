4dd51a452bfad9584bc6251144f30f74e390c81f
fabio <fabiopozzi@fastmail.com>
2017-02-05 15:03:22 +0100

    Complete work to get sum of expenses for a given expense category

--- draw_data.rb
+++ draw_data.rb
@@ -3,6 +3,19 @@ require 'csv'
 require 'date'
 require 'chartkick'
 
+def load_csv_file filename
+  data = []
+  CSV.foreach(filename, col_sep: ';', headers: true) do |row|
+    t = {}
+    t[:titolo] = row['Titolo']
+    t[:data] = Date.strptime(row['Data'], "%d\/%m\/%Y")
+    t[:categoria] = row['Categoria principale']
+    t[:importo] = row['Importo']
+    data << t
+  end
+  data
+end
+
 # takes a date and data array
 # returns a new array containing rows with matching year and month
 def filter_by_month(date, records)
@@ -12,44 +25,39 @@ def filter_by_month(date, records)
 end
 
 def totale_hobby_per_mese records
+  entries = []
   (1..12).each do |i|
     d = filter_by_month(Date.new(2016, i, 1), records)
     hobby = d.select { |x| x[:categoria] == "Sport e tempo libero" }
-    puts hobby.inspect
+    entries << [i, totale_per_mese(hobby)]
   end
+  entries
+end
+
+def totale_per_mese dati_mese
+    tot = dati_mese.inject(0) {|sum, x| sum + x[:importo].to_i.abs}
 end
 
 def numero_entry_per_mese records
   dati_mese = []
 
   (1..12).each do |i|
-    dati_mese << [i, filter_by_month(Date.new(2016, i, 1), records).count]
+    cur_month = Date.new(2016, i, 1)
+    dati_mese << [i, filter_by_month(cur_month, records).count]
   end
   dati_mese
 end
 
-def load_csv_file filename
-  data = []
-  CSV.foreach(filename, col_sep: ';', headers: true) do |row|
-    t = {}
-    t[:titolo] = row['Titolo']
-    t[:data] = Date.strptime(row['Data'], "%d\/%m\/%Y")
-    t[:categoria] = row['Categoria principale']
-    data << t
-  end
-  data
-end
-
 dati_grezzi = load_csv_file('./dati/aaa.csv')
-# filtro per nome titolo
-# tim_array = data.map{ |x| x if x[:titolo] == 'tim' }
-# puts tim_array.compact
-dati_mese = numero_entry_per_mese dati_grezzi
+dati_mese = numero_entry_per_mese(dati_grezzi)
 
-totale_hobby_per_mese dati_grezzi
+tot_hobby = totale_hobby_per_mese(dati_grezzi)
+#puts dati_mese.inspect
+#puts tot_hobby.inspect
 
 get '/' do
-  @titolo1 = "Numero entry per mese"
-  erb :index, :locals => {:dati_mese => dati_mese}
+  @titolo1 = "Numero spese"
+  @titolo2 = "Totale spese 'hobby'"
+  erb :index, :locals => {:dati_mese => dati_mese, :dati_hobby => tot_hobby}
 end
 
