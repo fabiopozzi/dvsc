37f878288fecd91893881eefb0fbf8d7ebbf6a1d
fabio <fabiopozzi@fastmail.com>
2017-02-05 15:03:11 +0100

    improve csv loading test

--- test_data.rb
+++ test_data.rb
@@ -2,7 +2,14 @@ require "minitest/autorun"
 require "./draw_data.rb"
 
 class DataTests < Minitest::Test
-  def test_load_file_returns_some_data
+
+  def test_load_file_returns_array
     assert_instance_of Array, load_csv_file('./dati/test.csv')
   end
+
+  def test_csv_array_length
+    d = load_csv_file('./dati/test.csv')
+    assert_equal 2, d.length
+  end
+
 end
